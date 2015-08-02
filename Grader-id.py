5a1c218ef5a61382d52f0e1d011afcac74bf60e9
Ingrid <ingridgg@ifi.uio.no>
2015-08-02 16:04:20 +0200

    Program for grading category paths, using ids

--- Grader-id.py
+++ Grader-id.py
@@ -1,6 +1,14 @@
+from __future__ import division
+
 import sys, os, numpy, re, fileinput, gzip, gc, time, Queue, collections
 from operator import itemgetter
 from myprint import myprint as p
+
+"""
+Program for grading all category paths. 
+The program uses ids instead of category names. 
+Top 3 category paths are stored for each article. 
+"""
 
 class Path(object): 
     def __init__(self, score, path): 
@@ -13,7 +21,6 @@ class Path(object):
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", \
                "t", "u", "v", "w", "x", "y", "z", "restfile"]
 
-#letters = ["restfile"]
 gradesfilename = "category-grade.txt.gz"
 idfilename = "id-mapper.txt.gz"
 
@@ -21,12 +28,10 @@ p("Reading all grades", "info")
 allgrades = dict()
 grades = dict()
 with gzip.open(gradesfilename, "rb") as gradefile: 
-#with open(gradesfilename, "r") as gradefile: 
     for line in gradefile: 
         line = line.strip()
         splittet = line.split("\t")
         category = splittet[0]
-        #print line
         grade = float(splittet[1])
         grades[category] = grade
 
@@ -46,23 +51,20 @@ with gzip.open(idfilename, "rb") as idfile:
         myentry = identry[1]
         
         id_to_name[myid] = myentry
-        #name_to_id[myentry] = myid
+        
 
 
-noofpathsperarticle = dict()
 for letter in letters: 
     inputfilename = letter + "id.txt.gz"
-    #outputfilename = "best-" + letter + ".txt.gz"
+    outputfilename = "best-" + letter + ".txt.gz"
     idoutputfilename = "best-" + letter + "-id.txt.gz"
-    articles_not_graded = dict()
+    
     artpaths = dict()
     p("Reading all categories for letter %s" %(letter), "info")
     with gzip.open(inputfilename, "rb") as inputfile: 
         for line in inputfile: 
             line = line.strip()
-
             if line.startswith("*"): 
-                # path found
                 path = line[1:]
                 if article in artpaths: 
                     artpaths[article][path] = 1
@@ -74,62 +76,47 @@ for letter in letters:
 
     p("Finished reading the whole file", "info")
     p("Grading all paths", "info")
-    with gzip.open("articlesnotgraded" + letter + ".txt.gz", "wb") as notgradedfile: 
+    with gzip.open(outputfilename, "wb") as outputfile: 
         with gzip.open(idoutputfilename, "wb") as idoutputfile: 
             for article in artpaths: 
                 que = Queue.PriorityQueue()
                 worse = 0
-                minpathscore = 100000
                 for artpath in artpaths[article]: 
                     pathscore = 0
                     cats_in_path = artpath.split("/")
+                    pathcatcnt = 0
                     for cat in cats_in_path: 
+                        pathcatcnt+=1
                         if cat in grades: 
                             pathscore += grades[cat]
                         else: 
                             print "%s: %s\n" %(pathscore, artpath)
-#                if pathscore in allgrades: 
-#                    allgrades[pathscore]+=1
-#                else: 
-#                    allgrades[pathscore] = 1
-                    
-                #if pathscore < 32.701703: 
-                    if pathscore < minpathscore: 
-                        minpathscore = pathscore
-                    if pathscore < 64.026493: 
-                   #if pathscore < 999.983648: 
+                    pathscore = pathscore/pathcatcnt
+                    if pathscore in allgrades: 
+                        allgrades[pathscore]+=1
+                    else: 
+                        allgrades[pathscore] = 1
+                    if que.qsize() < 3: 
                         que.put(Path(pathscore, artpath))
                         if worse < pathscore: 
                             worse = pathscore
-                        elif pathscore < worse: 
-                            removed = que.get()
-                            que.put(Path(pathscore, artpath))
-                            notremoved = que.get()
-                            worse = notremoved.score
-                            que.put(notremoved)
+                    elif pathscore < worse: 
+                        removed = que.get()
+                        que.put(Path(pathscore, artpath))
+                        notremoved = que.get()
+                        worse = notremoved.score
+                        que.put(notremoved)
                 
-                #idoutputfile.write("%s:\n" %(article))
+                outputfile.write("%s:\n" %(id_to_name[article]))
+                idoutputfile.write("%s:\n" %(article))
                 mypaths = []
                 while(que.empty() == False): 
                     mypaths.insert(0, que.get())
-                if len(mypaths) == 0:
-                    articles_not_graded[article] = minpathscore
-                    #notgradedfile.write("%s (%d)\n" %(article, minpathscore))
-                noofpaths = len(mypaths)
-                if noofpaths in noofpathsperarticle: 
-                    noofpathsperarticle[noofpaths] += 1
-                else: 
-                    noofpathsperarticle[noofpaths] = 1
                 for goodpath in mypaths: 
-                    a =  0
-                    #idoutputfile.write("*%s (%s)\n" %(goodpath.path, goodpath.score))
-        notgradedfile.close()
-    idoutputfile.close()
-    p("Articles not graded for %s is %d" %(letter, len(articles_not_graded)), "info")
-
-p("Number of paths found for all articles", "info")
-with open("noofpaths.txt", "w") as pathfile: 
-    for number in noofpathsperarticle: 
-        pathfile.write("%s\t%d\n" %(number, noofpathsperarticle[number]))
-pathfile.close()
+                    idoutputfile.write("*%s (%s)\n" %(goodpath.path, goodpath.score))
+                    outputfile.write("* %s\t" %(goodpath.score))
+                    for art in goodpath.path.split("/"):
+                        outputfile.write("%s/" %(id_to_name[art]))
+                    outputfile.write("\n")
+        outputfile.close()
 gc.collect()
